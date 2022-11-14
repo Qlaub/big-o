d4e13a7971cd186057510476757d23525f111094
Alex Glaubitz <e.pirazzi@gmail.com>
2022-11-13 22:54:24 -0500

    selection sort

--- pages/functions/sortingAlgorithms.ts
+++ pages/functions/sortingAlgorithms.ts
@@ -1,4 +1,4 @@
-// Compares element to next element and swaps if necessary
+// Compares each element to next element and swaps if necessary
 export const bubbleSort = (data: Array<number>) => {
   const length = data.length;
 
@@ -20,3 +20,25 @@ export const bubbleSort = (data: Array<number>) => {
   return data;
 };
 
+// Compares each number to every number after it and swaps with lowest number
+export const selectionSort = (data: Array<number>) => {
+  const length = data.length;
+
+  for (let i = 0; i < length; i++) {
+    let minIndex = i;
+
+    for (let n = i + 1; n < length; n++) {
+      if (data[n] < data[minIndex]) {
+        minIndex = n;
+      }
+    }
+
+    if (minIndex !== i) {
+      const hold = data[i];
+      data[i] = data[minIndex];
+      data[minIndex] = hold;
+    }
+  }
+
+  return data;
+};
