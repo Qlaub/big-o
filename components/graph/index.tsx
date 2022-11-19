99033f539da7eb65b529239202f3f97242610dfb
Alex Glaubitz <e.pirazzi@gmail.com>
2022-11-18 19:21:51 -0500

    graph dataset handling to do

--- components/graph/index.tsx
+++ components/graph/index.tsx
@@ -34,6 +34,20 @@ interface GraphProps {
 }
 
 export default function Graph({ sort, n, time, addPoint, setAddPoint }: GraphProps) {
+  // Sets up datasets equal to the number of sort algorithms
+  // TODO: only add a dataset after data has been recorded
+  // This will minimize screen clutter
+  // Initialize datasets as empty array
+  // When data comes in, check for dataset name in datasets
+  // If no dataset present, create dataset 
+  // Push the new plot point to our data subarray
+  // Return setDatasets(newDataset)
+  // If dataset IS present, check if data needs sorting (is new x lower than highest x value so far?)
+  // If data needs sorting, sort all data within dataset by x value from lowest to highest
+  // Push sorted data into new dataset
+  // Return setDatasets(newDataset)
+  // If data needs no sorting, create newDataset as copy of old dataset and push new data into data subarray
+  // Return setDatasets(newDataset)
   const [datasets, setDatasets] = useState<Array<DatasetObject>>(
     data.map(({ name, borderColor, backgroundColor }) => {
       return {
@@ -60,7 +74,7 @@ export default function Graph({ sort, n, time, addPoint, setAddPoint }: GraphPro
     // Insert new data into object array at correct index
     newDataset[index].data.push({x: n, y: time});
 
-    // TO-DO: re-sort the datapoints if necessary before pushing
+    // TO-DO: re-sort the data points if necessary before pushing
 
     setDatasets(newDataset);
   }, [n, time, sort, datasets]);
